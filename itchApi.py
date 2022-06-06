d872d7a83eef6ab6715ef3d3f5c1daa20ebb3560
Huntrox <abde.had@hotmail.com>
2022-06-06 19:00:52 +0100

    init

---
+++ itchApi.py
@@ -0,0 +1,101 @@
+from typing import List
+from typing import Any
+from dataclasses import dataclass
+import json
+import asyncio
+import requests 
+import subprocess
+
+
+class ItchApi:
+    def __init__(self, url):
+        self.url = url
+        self.root = None
+        self.games = []
+
+    async def get_games(self):
+        
+        loop = asyncio.get_event_loop()
+        future = loop.run_in_executor(None, requests.get, self.url)
+        response = await future
+        # generate the object using json.dumps()
+        # corresponding to json data
+        json_data = json.loads(response.text)
+        self.root = Root.from_dict(json_data)
+        # loop through root.games and print game id and title
+        for game in self.root.games:
+            username = game.user.username;
+            game_id = game.url.split(f'itch.io/')[1]
+            str = f"{username}/{game_id}"
+            self.games.append(str)
+        return self.root.games
+    
+    async def upload_build(self, build_path, game_page, channel, version):
+        butler_command = f'butler push {build_path} {game_page}:{channel} --userversion {version}'
+        cmd = f'cmd.exe /k "{butler_command}"'
+
+        output = subprocess.Popen(['start','cmd','/k',butler_command], shell = True, stdin = subprocess.PIPE, stdout = subprocess.PIPE, text = True)
+
+        return output.stdout
+
+
+@dataclass
+class User:
+    id: int
+    url: str
+    cover_url: str
+    username: str
+    display_name: str
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'User':
+        _id = int(obj.get("id"))
+        _url = str(obj.get("url"))
+        _cover_url = str(obj.get("cover_url"))
+        _username = str(obj.get("username"))
+        _display_name = str(obj.get("display_name"))
+        return User(_id, _url, _cover_url, _username, _display_name)
+
+
+@dataclass
+class Game:
+    user: User
+    p_android: bool
+    published: bool
+    created_at: str
+    p_windows: bool
+    p_osx: bool
+    p_linux: bool
+    type: str
+    title: str
+    url: str
+    id: int
+    cover_url: str
+    published_at: str
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'Game':
+        _user = User.from_dict(obj.get("user"))
+        _p_android = bool(obj.get("p_android"))
+        _published = bool(obj.get("published"))
+        _created_at = str(obj.get("created_at"))
+        _p_windows = bool(obj.get("p_windows"))
+        _p_osx = bool(obj.get("p_osx"))
+        _p_linux = bool(obj.get("p_linux"))
+        _type = str(obj.get("type"))
+        _title = str(obj.get("title"))
+        _url = str(obj.get("url"))
+        _id = int(obj.get("id"))
+        _cover_url = str(obj.get("cover_url"))
+        _published_at = str(obj.get("published_at"))
+        return Game(_user,  _p_android,   _published, _created_at,  _p_windows, _p_osx, _p_linux, _type, _title, _url,  _id, _cover_url, _published_at)
+
+
+@dataclass
+class Root:
+    games: List[Game]
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'Root':
+        _games = [Game.from_dict(y) for y in obj.get("games")]
+        return Root(_games)
---
+++ main.py
@@ -0,0 +1,170 @@
+import asyncio
+import itchApi
+import tkinter as tk
+from tkinter import  StringVar, ttk, filedialog, scrolledtext
+
+padding = {'padx': 4, 'pady': 4}
+padding_btn = {'padx': 4, 'pady': 2}
+padding_label = {'padx': 3, 'pady': 4}
+padding_entry = {'padx': 4, 'pady': 4}
+
+EWN ="EWN"
+SEWN ="SEWN"
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.geometry("300x320")
+        self.title("Build Patcher")
+        self.resizable(True, False)
+        
+            
+        self.iconbitmap(r'icon.ico')
+        self.build_path_input = StringVar()
+        self.game_page_option = StringVar()
+        self.channel_option = StringVar()
+        self.build_version_input = StringVar()
+        self.console_output = StringVar()
+        self.console = scrolledtext.ScrolledText(
+            self, wrap=tk.WORD, width=40, height=1.5, font=("Helvetica", 10))
+
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=3)
+        self.columnconfigure(2, weight=1)
+
+        self.style = ttk.Style(self)
+        self.style.configure('TLabel', font=('Helvetica', 11, 'bold'))
+        self.style.configure('TButton', font=('Helvetica', 11))
+        self.style.configure('TEntry', font=('Helvetica', 11))
+        
+
+        self.api = itchApi.ItchApi(preferences.url)
+        asyncio.run(self.api.get_games())
+        # make array of api.games username and channels
+        self.game_pages = self.api.games
+        self.game_page_option.set(self.game_pages[0])
+
+        self.layer = 0;
+        
+        self.draw_path_container()
+        self.draw_game_page_container()
+        self.draw_channel_container()
+        self.draw_version_content()
+        self.draw_footer()
+
+
+    def draw_path_container(self):
+        
+        tk.Frame(self, height=1, bg="black").grid(
+            column=0,columnspan=3, row=self.layer, sticky='EWN',**padding)
+        self.layer_indent()
+        ttk.Label(self, text="Build Path: ", style="BW.TLabel").grid(
+            column=0, row=self.layer, sticky='EWN',**padding_label)
+        # self.build_path_input.set(preferences.butlerPath)
+        self.layer_indent()
+        
+        path_entry_field = ttk.Entry(self, textvariable=self.build_path_input)
+        path_entry_field.grid(row=self.layer, column=0, columnspan=2,
+                              sticky=EWN,**padding_entry)
+        
+        path_button = ttk.Button(self, text="Browse", command=self.browse_path)
+        path_button.grid(row=self.layer,column=2, sticky=EWN, **padding_btn)
+
+    def draw_game_page_container(self):
+        
+        self.layer_indent()
+        ttk.Label(self, text="Game Page: ", style="BW.TLabel").grid(
+            column=0, row=self.layer, sticky=EWN, **padding_label)
+        self.layer_indent()
+        game_page_menu = ttk.Combobox(
+            self, textvariable=self.game_page_option, values=self.game_pages)
+        game_page_menu.grid(row=self.layer, column=0, columnspan=3,
+                            sticky=EWN, **padding)
+        game_page_menu['state'] = 'readonly'
+
+    def draw_channel_container(self):
+        
+        self.layer_indent()
+        ttk.Label(self, text="Channel: ", style="BW.TLabel").grid(
+            column=0, row=self.layer, sticky=EWN, **padding_label)
+        opt = ["Win", "Mac", "Linux"]
+
+        self.channel_option.set(opt[0])
+        self.layer_indent()
+        channel_option_menu = ttk.Combobox(
+            self, textvariable=self.channel_option, values=opt)
+        channel_option_menu.grid(
+            row=self.layer, column=0, columnspan=3, sticky=EWN, **padding)
+        channel_option_menu['state'] = 'readonly'
+
+    def draw_version_content(self):
+        
+        self.layer_indent()
+        label = ttk.Label(self, text="Version: ", style="BW.TLabel").grid(
+            column=0, row=self.layer, sticky=EWN, **padding_label)
+        self.build_version_input.set("1.0.0")
+        self.layer_indent()
+        version_entry_field = ttk.Entry(self, textvariable=self.build_version_input).grid(
+            row=self.layer, column=0, columnspan=3, sticky=EWN, **padding)
+
+    def draw_footer(self):
+        self.layer_indent(2)
+        ttk.Button(self, text="Upload Build", command=self.on_build_btn).grid(
+            column=0, row=self.layer, columnspan=3, ipady=20, sticky=SEWN, **padding_btn)
+        
+        progressbar = ttk.Progressbar(self, orient=tk.HORIZONTAL, length=200, mode='determinate')
+
+    def on_build_btn(self):
+        asyncio.run(self.on_build())
+
+    def get_console_output(self, value):
+        self.console.config(state="normal")
+        self.console.delete(1.0, "end")
+        self.console.insert(1.0, value)
+        self.console.config(state="disabled")
+        self.console.update()
+
+    async def on_build(self):
+        version = self.build_version_input.get()
+        build_path = self.build_path_input.get()
+        channel = self.channel_option.get()
+        game_page = self.game_page_option.get()
+        
+        output = await self.api.upload_build(build_path, game_page, channel, version)
+        self.console_output.set(output)
+        self.get_console_output(output)
+        
+    def layer_indent(self,height = 1): 
+        self.layer += height
+
+    def browse_path(self):
+        file_path = filedialog.askopenfilename(
+            initialdir="/", title="Select file", filetypes=(("Zip files", "*.zip"),("Rar files","*.rar"),("all files", "*.*")))
+        self.build_path_input.set(file_path)
+
+class Preferences:
+    def __init__(self):
+        self.apiToken = None
+        self.butlerPath = None
+        #load prefs file from prefs path and read it using file stream
+        with open("preferences.txt", 'r') as preferences_file:
+            #read the file line by line
+            for line in preferences_file:
+                #split the line by the = sign
+                splitLine = line.split('=')
+                #if the first part of the line is the api token
+                if splitLine[0] == 'apiToken':
+                    #set the api token to the second part of the line
+                    self.apiToken = splitLine[1].strip()
+                #if the first part of the line is the butler path
+                # elif splitLine[0] == 'butlerPath':
+                #     #set the butler path to the second part of the line
+                #     self.butlerPath = splitLine[1].strip()       
+        self.url = f'https://itch.io/api/1/{self.apiToken}/my-games'
+  
+preferences = Preferences()
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
